8a170d70df9fa0ef0c5a1c0759de72d73e8af7d8
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2022-11-07 21:58:50 +0200

    Tehtävä 10

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,9 +1,16 @@
 from kauppa import Kauppa
+from pankki import Pankki
+from viitegeneraattori import Viitegeneraattori
+from varasto import Varasto
 from kirjanpito import Kirjanpito
 
 
 def main():
-    kauppa = Kauppa()
+    kauppa = Kauppa(
+        Varasto.get_instance(),
+        Pankki.get_instance(),
+        Viitegeneraattori.get_instance()
+    )
 
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
