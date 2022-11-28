6446c9bc1aa371ead18e0691b8d9d8f1b5014dd2
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2022-11-28 19:18:54 +0200

    Osa 10

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -72,3 +72,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.hinta(), 3)
         self.assertEqual(ostos.lukumaara(), 1)
+
+    # osa 10
+    def test_kahden__eri_tuotteen_lisaaminen_len(self):
+        maito = Tuote("Maito", 3)
+        suklaa = Tuote("Suklaa", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(suklaa)
+        self.assertEqual(len(self.kori.ostokset()), 2)
