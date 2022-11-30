7fa641842f780b5e076319ed3375734cd70b4037
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2022-11-30 23:18:03 +0200

    Heavily codegolf the code to shrink it to 10 rows

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,16 +1,10 @@
+g = [['Love', 'Fifteen', 'Thirty', 'Forty'], 'Deuce', ['Advantage ', 'Win for ']]
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.players = [(player1_name, 0), (player2_name, 0)]
-
-    def won_point(self, player_name):
-        idx = list(map(lambda p: p[0], self.players)).index(player_name)
-        self.players[idx] = (player_name, self.players[idx][1] + 1)
-
-    def get_score(self):
-        winning = max(self.players, key=lambda p: p[1])
-        diff = abs(self.players[0][1] - self.players[1][1])
-        if winning[1] < 4:
-            if diff == 0:
-                return f"{['Love', 'Fifteen', 'Thirty', 'Forty'][winning[1]]}-All"
-            return "-".join([['Love', 'Fifteen', 'Thirty', 'Forty'][player[1]] for player in self.players])
-        return f"Win for {winning[0]}" if diff > 1 else ["Deuce", f"Advantage {winning[0]}"][diff]
+        [self.p, self.s] = [[player1_name, player2_name], [0, 0]]
+    def won_point(self, pn):
+        self.s[self.p.index(pn)] += 1
+    def get_score(t):
+        if max(t.s) < 4:
+            return f"{[g[0][i] for i in t.s][0]}-All" if t.s[0] == t.s[1] else "-".join([g[0][i] for i in t.s])
+        return g[1] if t.s[0] == t.s[1] else g[2][min(max(t.s) - min(t.s) - 1, 1)] + t.p[t.s.index(max(t.s))]
